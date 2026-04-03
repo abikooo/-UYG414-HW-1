e359cf88953b2c83995aa0740839c2e49a8d0dd9
abikooo <seiilbekabylay@gmail.com>
2026-04-03 11:27:09 +0300

    Fix CI by making logger resilient to missing logstash host

--- project/notification_service/logger_setup.py
+++ project/notification_service/logger_setup.py
@@ -5,20 +5,25 @@ def setup_app_logging(service_name: str):
     logger = logging.getLogger(service_name)
     logger.setLevel(logging.INFO)
 
-    # Logstash handler (TCP port 5044 as configured in logstash.conf)
-    logstash_handler = AsynchronousLogstashHandler(
-        host='logstash', 
-        port=5044, 
-        database_path=None
-    )
-    
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logstash_handler.setFormatter(formatter)
-    
-    logger.addHandler(logstash_handler)
+    try:
+        # Logstash handler (TCP port 5044 as configured in logstash.conf)
+        # We use a try-except because in CI/test environments, host 'logstash' might not resolve
+        logstash_handler = AsynchronousLogstashHandler(
+            host='logstash', 
+            port=5044, 
+            database_path=None
+        )
+        
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        logstash_handler.setFormatter(formatter)
+        logger.addHandler(logstash_handler)
+    except Exception:
+        # Fallback to standard logging if Logstash is unreachable or host resolution fails
+        pass
     
     # Also log to console
     console_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
     
